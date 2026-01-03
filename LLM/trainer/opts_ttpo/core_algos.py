669e2ccf2415588c3d52846ad296d82415dface4
lu-jun-yu <jun_yu_lu@163.com>
2026-01-03 15:32:57 +0800

    restore lam decay

--- LLM/trainer/opts_ttpo/core_algos.py
+++ LLM/trainer/opts_ttpo/core_algos.py
@@ -2213,8 +2213,8 @@ def select_next_states(
 
     # 3) Compute TUCT: exploitation * lam_t + exploration
     exploration = c * torch.sqrt(torch.log(partree_branches[:, :-1])) / (subtree_branches[:, :-1] + 1e-8)
-    # tuct = expected_traj_reward * lam_t[:, 1:] + exploration
-    tuct = expected_traj_reward + exploration
+    tuct = expected_traj_reward * lam_t[:, 1:] + exploration
+    # tuct = expected_traj_reward + exploration
     tuct = torch.where(response_mask[:, :-1] > 0, tuct, torch.tensor(-float('inf')))
 
     # Mask positions that would exceed max_prompt_length
