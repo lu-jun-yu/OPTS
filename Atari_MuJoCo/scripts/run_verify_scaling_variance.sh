d64692ee79577b70baf0f2e9b36980acc7ea9c5d
lu-jun-yu <jun_yu_lu@163.com>
2026-04-01 09:22:01 +0800

    update scaling scripts

--- Atari_MuJoCo/scripts/run_verify_scaling_variance.sh
+++ Atari_MuJoCo/scripts/run_verify_scaling_variance.sh
@@ -11,7 +11,7 @@ for task in HalfCheetah-v4 Walker2d-v4 Hopper-v4 Ant-v4 Humanoid-v4; do
             --env-id $task \
             --seed $seed \
             --total-steps 1000000 \
-            --num-steps 10000 \
+            --num-steps 100000 \
             --batch-sizes 64,128,256,512,1024,2048,4096 \
             --num-bootstrap 100 \
             --max-search-per-tree 4 \
