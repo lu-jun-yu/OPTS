9f1210367e8dce5e0b97afa6dd8b2308a921744a
lu-jun-yu <jun_yu_lu@163.com>
2026-05-10 16:40:03 +0800

    align parameter

--- LLM/scripts/run_reinforce_pp_baseline.sh
+++ LLM/scripts/run_reinforce_pp_baseline.sh
@@ -23,7 +23,7 @@ python3 -m verl.trainer.main_ppo \
  actor_rollout_ref.rollout.name=vllm \
  actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=128 \
  actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
- actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+ actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
  actor_rollout_ref.rollout.n=4 \
  actor_rollout_ref.rollout.val_kwargs.n=32 \
  actor_rollout_ref.rollout.val_kwargs.do_sample=True \
